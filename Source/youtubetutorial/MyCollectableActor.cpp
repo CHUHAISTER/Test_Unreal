63457449d16b05a3e0d328c019c3fb4d5bc4795a
CHUHAISTER <92538081+CHUHAISTER@users.noreply.github.com>
2025-01-06 16:56:35 +0300

    Add Jump

---
+++ Source/youtubetutorial/MyCollectableActor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCollectableActor.h"
+
+// Sets default values
+AMyCollectableActor::AMyCollectableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	//Create the (root) component for rendering
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	RootComponent = StaticMesh;
+}
+
+void AMyCollectableActor::Jump(float velocity)
+{
+	StaticMesh->AddImpulse({.0f, .0f, velocity * 500.f});
+}
+
+// Called when the game starts or when spawned
+void AMyCollectableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCollectableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
